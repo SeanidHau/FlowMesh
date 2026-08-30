69a6f6c642bb2d2144af64bddddca8ee3548ff1a
Seanid <shigureli@seanid--MacBook-Pro.local>
2026-08-30 23:02:58 +0800

    chore: ignore electron build output

--- frontend/dist-electron/preload.js
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-/**
- * 只向本地渲染进程暴露受限的 API 请求能力，不暴露 Node 或 Electron 原始对象。
- */
-contextBridge.exposeInMainWorld('flowmesh', {
-    request: (request) => ipcRenderer.invoke('flowmesh:request', request),
-});
